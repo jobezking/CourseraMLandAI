4a83ff001f6bb95e5f01e8d724f50d15c073dfcf
Job King <jobezking@gmail.com>
2025-03-26 00:10:05 -0400

    done

--- deeplearning.ai/supervised_ml/classification_linear_regression.py
+++ deeplearning.ai/supervised_ml/classification_linear_regression.py
@@ -1,11 +1,13 @@
+import numpy as np
+import math
+import copy
+
 '''
 fw,b(X)  where w and X are vectors
 z = w ⋅ x + b
 where
 g(z) = 1 / (1+e**-z)
 '''
-
-import numpy as np
 
 def sigmoid(z):
 #Compute the sigmoid of z
@@ -14,3 +16,285 @@ def sigmoid(z):
          
     g = 1/(1+np.exp(-z))
     return g
+
+'''
+In logistic regression the cost function is:
+              m-1
+J(w,b) = 1/m Σ [loss(fw,b(x_s(i)), y_s(i)]
+              i=0
+where loss(fw,b(x_s(i)),y_s(i) is the cost for a single data point which is:
+loss(fw,b(x_s(i)), y_s(i)) = -y_s(i)log(fw,b(x_s(i)) - (1 - y_s(i))log(fw,b(x_s(i)) )
+without the [i] index for readability
+loss(fw,b(x), y) = -y log(fw,b(x)) - (1 - y)log(1 - fw,b(x))
+Note that below is supposed to have _s(i) as it is also i = 0 to m-1
+fw,b(x) = g(z)
+z = w ⋅ x + b
+g(z) = 1 / (1+e**-z)
+'''
+
+def compute_cost_logistic(X, y, w, b):
+    """
+    Computes cost
+
+    Args:
+      X (ndarray (m,n)): Data, m examples with n features
+      y (ndarray (m,)) : target values
+      w (ndarray (n,)) : model parameters  
+      b (scalar)       : model parameter
+      
+    Returns:
+      cost (scalar): cost
+    """
+
+    m = X.shape[0]
+    cost = 0.0
+    for i in range(m):
+        z_i = np.dot(X[i],w) + b
+        f_wb_i = sigmoid(z_i)
+        cost +=  -y[i]*np.log(f_wb_i) - (1-y[i])*np.log(1-f_wb_i)
+             
+    cost = cost / m
+    return cost
+
+'''
+gradient for multiple variables means j = 0..n - 1
+
+                   m-1
+dJ(w,b) / dw = 1/m Σ (fwb(x_s(i)) - y_s(i)) * x
+                   i = 0
+
+                   m-1
+dJ(w,b) / db = 1/m Σ (fwb(x_s(i)) - y_s(i))
+                   i = 0
+
+which equates to
+
+                   m-1
+dJ(w,b) / dw = 1/m Σ (wx_s(i) + b - y_s(i)) * x
+                   i = 0
+
+                   m-1
+dJ(w,b) / db = 1/m Σ (wx_s(i) + b - y_s(i))
+                   i = 0
+
+fw,b(x) = g(z)
+z = w ⋅ x + b
+g(z) = 1 / (1+e**-z)                   
+'''
+
+def compute_gradient_logistic(X, y, w, b): 
+    """
+    Computes the gradient for logistic regression 
+ 
+    Args:
+      X (ndarray (m,n): Data, m examples with n features
+      y (ndarray (m,)): target values
+      w (ndarray (n,)): model parameters  
+      b (scalar)      : model parameter
+    Returns
+      dj_dw (ndarray (n,)): The gradient of the cost w.r.t. the parameters w. 
+      dj_db (scalar)      : The gradient of the cost w.r.t. the parameter b. 
+    """
+    m,n = X.shape
+    dj_dw = np.zeros((n,))                           #(n,)
+    dj_db = 0.
+
+    for i in range(m):
+        f_wb_i = sigmoid(np.dot(X[i],w) + b)          #(n,)(n,)=scalar
+        err_i  = f_wb_i  - y[i]                       #scalar
+        for j in range(n):
+            dj_dw[j] = dj_dw[j] + err_i * X[i,j]      #scalar
+        dj_db = dj_db + err_i
+    dj_dw = dj_dw/m                                   #(n,)
+    dj_db = dj_db/m                                   #scalar
+        
+    return dj_db, dj_dw  
+
+'''
+gradient descent to find ideal values for w and b using iteration
+{ 
+w = w - α * dJ(w,b) / dw   
+b = b - α * dJ(w,b) / dw 
+} for j = 0..n - 1'
+'''
+
+def gradient_descent_logistic(X, y, w_in, b_in, alpha, num_iters): 
+    """
+    Performs batch gradient descent
+    
+    Args:
+      X (ndarray (m,n)   : Data, m examples with n features
+      y (ndarray (m,))   : target values
+      w_in (ndarray (n,)): Initial values of model parameters  
+      b_in (scalar)      : Initial values of model parameter
+      alpha (float)      : Learning rate
+      num_iters (scalar) : number of iterations to run gradient descent
+      
+    Returns:
+      w (ndarray (n,))   : Updated values of parameters
+      b (scalar)         : Updated value of parameter 
+    """
+    # An array to store cost J and w's at each iteration primarily for graphing later
+    J_history = []
+    w = copy.deepcopy(w_in)  #avoid modifying global w within function
+    b = b_in
+    
+    for i in range(num_iters):
+        # Calculate the gradient and update the parameters
+        dj_db, dj_dw = compute_gradient_logistic(X, y, w, b)   
+
+        # Update Parameters using w, b, alpha and gradient
+        w = w - alpha * dj_dw               
+        b = b - alpha * dj_db               
+      
+        # Save cost J at each iteration
+        if i<100000:      # prevent resource exhaustion 
+            J_history.append( compute_cost_logistic(X, y, w, b) )
+
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i% math.ceil(num_iters / 10) == 0:
+            print(f"Iteration {i:4d}: Cost {J_history[-1]}   ")
+        
+    return w, b, J_history         #return final w,b and J history for graphing
+
+'''
+In logistic regression the cost function is with regularization is MSE plus lambda (regularization term):
+              m-1                                    n-1
+J(w,b) = 1/2m Σ (fwb(x_s(i)) - y_s(i))**2 + lamba/2m Σ ω**2j_s(i)
+              i=0                                    j=0
+or for readability
+              m-1                         n-1
+J(w,b) = 1/2m Σ (fwb(x) - y)**2 + lamba/2m Σ ω**2j
+              i=0                         j=0
+where
+fw,b(x) = w ⋅ x + b
+'''
+def compute_cost_linear_reg(X, y, w, b, lambda_ = 1):
+    """
+    Computes the cost over all examples
+    Args:
+      X (ndarray (m,n): Data, m examples with n features
+      y (ndarray (m,)): target values
+      w (ndarray (n,)): model parameters  
+      b (scalar)      : model parameter
+      lambda_ (scalar): Controls amount of regularization
+    Returns:
+      total_cost (scalar):  cost 
+    """
+
+    m  = X.shape[0]
+    n  = len(w)
+    cost = 0.
+    for i in range(m):
+        f_wb_i = np.dot(X[i], w) + b                                   #(n,)(n,)=scalar, see np.dot
+        cost = cost + (f_wb_i - y[i])**2                               #scalar             
+    cost = cost / (2 * m)                                              #scalar  
+ 
+    reg_cost = 0
+    for j in range(n):
+        reg_cost += (w[j]**2)                                          #scalar
+    reg_cost = (lambda_/(2*m)) * reg_cost                              #scalar
+    
+    total_cost = cost + reg_cost                                       #scalar
+    return total_cost                                                  #scalar
+
+'''                                                                                                              j-1
+regularized logistic regression cost function is also the regular logistic regression cost function plus lamba/2m Σ ω**2j_s(i)
+                                                                                                                  n=0
+                                                                                                                  
+'''
+def compute_cost_logistic_reg(X, y, w, b, lambda_ = 1):
+    """
+    Computes the cost over all examples
+    Args:
+    Args:
+      X (ndarray (m,n): Data, m examples with n features
+      y (ndarray (m,)): target values
+      w (ndarray (n,)): model parameters  
+      b (scalar)      : model parameter
+      lambda_ (scalar): Controls amount of regularization
+    Returns:
+      total_cost (scalar):  cost 
+    """
+
+    m,n  = X.shape
+    cost = 0.
+    for i in range(m):
+        z_i = np.dot(X[i], w) + b                                      #(n,)(n,)=scalar, see np.dot
+        f_wb_i = sigmoid(z_i)                                          #scalar
+        cost +=  -y[i]*np.log(f_wb_i) - (1-y[i])*np.log(1-f_wb_i)      #scalar
+             
+    cost = cost/m                                                      #scalar
+
+    reg_cost = 0
+    for j in range(n):
+        reg_cost += (w[j]**2)                                          #scalar
+    reg_cost = (lambda_/(2*m)) * reg_cost                              #scalar
+    
+    total_cost = cost + reg_cost                                       #scalar
+    return total_cost                                                  #scalar
+
+#######
+def compute_gradient_linear_reg(X, y, w, b, lambda_): 
+    """
+    Computes the gradient for linear regression 
+    Args:
+      X (ndarray (m,n): Data, m examples with n features
+      y (ndarray (m,)): target values
+      w (ndarray (n,)): model parameters  
+      b (scalar)      : model parameter
+      lambda_ (scalar): Controls amount of regularization
+      
+    Returns:
+      dj_dw (ndarray (n,)): The gradient of the cost w.r.t. the parameters w. 
+      dj_db (scalar):       The gradient of the cost w.r.t. the parameter b. 
+    """
+    m,n = X.shape           #(number of examples, number of features)
+    dj_dw = np.zeros((n,))
+    dj_db = 0.
+
+    for i in range(m):                             
+        err = (np.dot(X[i], w) + b) - y[i]                 
+        for j in range(n):                         
+            dj_dw[j] = dj_dw[j] + err * X[i, j]               
+        dj_db = dj_db + err                        
+    dj_dw = dj_dw / m                                
+    dj_db = dj_db / m   
+    
+    for j in range(n):
+        dj_dw[j] = dj_dw[j] + (lambda_/m) * w[j]
+
+    return dj_db, dj_dw
+
+#####
+def compute_gradient_logistic_reg(X, y, w, b, lambda_): 
+    """
+    Computes the gradient for linear regression 
+ 
+    Args:
+      X (ndarray (m,n): Data, m examples with n features
+      y (ndarray (m,)): target values
+      w (ndarray (n,)): model parameters  
+      b (scalar)      : model parameter
+      lambda_ (scalar): Controls amount of regularization
+    Returns
+      dj_dw (ndarray Shape (n,)): The gradient of the cost w.r.t. the parameters w. 
+      dj_db (scalar)            : The gradient of the cost w.r.t. the parameter b. 
+    """
+    m,n = X.shape
+    dj_dw = np.zeros((n,))                            #(n,)
+    dj_db = 0.0                                       #scalar
+
+    for i in range(m):
+        f_wb_i = sigmoid(np.dot(X[i],w) + b)          #(n,)(n,)=scalar
+        err_i  = f_wb_i  - y[i]                       #scalar
+        for j in range(n):
+            dj_dw[j] = dj_dw[j] + err_i * X[i,j]      #scalar
+        dj_db = dj_db + err_i
+    dj_dw = dj_dw/m                                   #(n,)
+    dj_db = dj_db/m                                   #scalar
+
+    for j in range(n):
+        dj_dw[j] = dj_dw[j] + (lambda_/m) * w[j]
+
+    return dj_db, dj_dw  
